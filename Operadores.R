2e3690fe5b97766474fa22c09e7633b7d40ef318
Maxuel Antenor Pedro <36570558+MaxuelAntenor@users.noreply.github.com>
2020-06-24 18:23:41 -0300

    Operadores - Linguagem R
    Instruções iniciais na Linguagem R - Operadores

---
+++ Operadores.R
@@ -0,0 +1,73 @@
+# Operadores
+
+# OBJETIVO: Realizar operações matemáticas, lógicas e de comparação
+
+# SOMA
+
+5 + 5
+
+# SUBTRAÇÃO
+
+10 - 8
+
+# DIVISÃO
+
+9 / 3
+
+# MULTIPLICAÇÃO
+
+4 * 2
+
+# POTÊNCIA
+
+4 ** 2
+4 ^ 2
+
+# IGUAL 
+
+5 == 6
+5 == 5
+
+# DIFERENTE
+
+5 != 6
+5 != 5
+
+# MAIOR
+
+4 > 2
+4 > 6
+
+# MENOR
+
+3 < 2
+3 < 4
+
+# Maior ou igual
+
+4 >= 5
+4 >= 4
+
+# Menor ou igual 
+
+6 <= 8
+6 <= 5
+
+# condicional &
+
+6 == 6
+6 ==6 & 7 ==8
+
+# condicional OU
+
+6 == 6
+7 == 8
+6 == 6 | 7 == 8
+
+6 != 6 | 7 == 8
+5 > 3 | 3 < 8
+
+# Negação
+
+!6 == 6
+!4 > 2
