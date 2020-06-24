0dbf8cc7f665ad35e6591424c519544586e97293
Maxuel Antenor Pedro <36570558+MaxuelAntenor@users.noreply.github.com>
2020-06-24 16:45:55 -0300

    Variáveis - Linguagem R
    Instruções iniciais na Linguagem R - Variáveis.

---
+++ Variaveis.R
@@ -0,0 +1,33 @@
+# Criando variáveis
+# Para executar clique sobre a linha ou selecione o conjunto de linhas
+# e pressione CTRL + ENTER
+
+# declarando o valor 10 para a variável a
+a <- 10
+a
+
+# colocando b sem declarar antes, depois declarando com valor 10
+# rodando a variável com letra maiúscula
+
+b
+b = 10
+b
+B
+
+# irei utilizar '<-' na criação de variáveis e '=' nas atribuições de funções
+
+c -> 10
+10 -> c
+c -> d
+
+# atribuindo a soma na variável 'c'
+c <- a + b
+
+a <- "Maxuel"
+b <- "João"
+
+# atribuindo a soma na variável 'c'
+c <- a + b
+
+
+
